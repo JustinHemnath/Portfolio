276f28c897236d3f37f5ebdb9f87cf7e5e5f6aa9
Justin Hemnath <justin.hemnath.96@gmail.com>
2022-08-27 00:06:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
-import { Provider } from 'react-redux';
-import Store from './Features/Store';
+import { Provider } from "react-redux";
+import Store from "./Features/Store";
 
 import App from "./App";
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
