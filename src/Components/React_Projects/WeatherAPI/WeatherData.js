f9a81e8200f5b5d087daee1ef751906b114d54b0
Justin Hemnath <justin.hemnath.96@gmail.com>
2022-09-01 14:31:48 +0530

    1/9

--- src/Components/React_Projects/WeatherAPI/WeatherData.js
+++ src/Components/React_Projects/WeatherAPI/WeatherData.js
@@ -5,26 +5,33 @@ import { motion } from "framer-motion";
 function WeatherData({ info }) {
   const { place, temp, tempMin, tempMax, climate } = info;
 
-  return ( 
+  return (
     <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }} 
-        className="weatherData">
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="weatherData"
+    >
       <div className="weather-div-1">
-        <b style={{ fontSize: "4rem", letterSpacing: 2 }}>{place}</b>
+        <b>{place}</b>
       </div>
 
       <div className="weather-div-2">
-        <b style={{ fontSize: "2rem" }}>{tempMin}<br />Min</b>
-        <b style={{ fontSize: "5.5rem" }}>{temp}</b>
-        <b style={{ fontSize: "2rem" }}>{tempMax}<br />Max</b>
+        <b className="tempSec">
+          {tempMin}
+          <br />
+          Min
+        </b>
+        <b className="tempPri">{temp}</b>
+        <b className="tempSec">
+          {tempMax}
+          <br />
+          Max
+        </b>
       </div>
 
       <div className="weather-div-3">
-        <b style={{ fontSize: "3rem", letterSpacing: 2 }}>
-              {climate}
-        </b>
+        <b>{climate}</b>
       </div>
     </motion.div>
   );
